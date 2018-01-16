0ab6a3a3d4e605675d8056322b9c5f51c372800d
Jonathan Reisdorf <jonathan.reisdorf@project-a.com>
2018-01-16 10:34:33 +0100

    small code changes WIP

--- scripts/app.jsx
+++ scripts/app.jsx
@@ -1,6 +1,6 @@
-"use strict";
-const bip39 = require( "bip39" );
-const bigi = require( 'bigi' )
+'use strict';
+const bip39 = require( 'bip39' );
+const bigi = require( 'bigi' );
 
 const React = require( 'react' );
 const ReactDOM = require( 'react-dom' );
@@ -18,121 +18,121 @@ const base58 = require( 'base-x' )( BASE58 );
 
 const comm_node = require('ledgerco/src/ledger-comm-node');
 
-process.stdout.write( "STARTED comm_node \n" );
-process.stdout.write( JSON.stringify(comm_node) + "\n" );
-process.stdout.write( comm_node + "\n" );
-process.stdout.write( Object.getOwnPropertyNames(comm_node) + "\n" );
-process.stdout.write( "SUCCESS comm_node \n" );
+process.stdout.write( 'STARTED comm_node \n' );
+process.stdout.write( JSON.stringify(comm_node) + '\n' );
+process.stdout.write( comm_node + '\n' );
+process.stdout.write( Object.getOwnPropertyNames(comm_node) + '\n' );
+process.stdout.write( 'SUCCESS comm_node \n' );
 
 const net = 'TestNet';
 
-const mnemonic = "online ramp onion faculty trap clerk near rabbit busy gravity prize employ exit horse found slogan effort dash siren buzz sport pig coconut element";
+const mnemonic = 'online ramp onion faculty trap clerk near rabbit busy gravity prize employ exit horse found slogan effort dash siren buzz sport pig coconut element';
 
 const bip44_path =
-    "8000002C"
-    + "80000378"
-    + "80000000"
-    + "80000000"
-    + "80000000";
+    '8000002C'
+    + '80000378'
+    + '80000000'
+    + '80000000'
+    + '80000000';
 
-var ledgerDeviceInfo = undefined;
+let ledgerDeviceInfo;
 
-var publicKeyInfo = undefined;
+let publicKeyInfo;
 
-var publicKey = undefined;
+let publicKey;
 
-var accounts = undefined;
+let accounts;
 
-var signature = undefined;
+let signature;
 
-var signatureInfo = undefined;
+let signatureInfo;
 
-var balance = undefined;
+let balance;
 
-var balanceInfo = undefined;
+let balanceInfo;
 
-var encodeTransactionResponse = undefined;
+let encodeTransactionResponse;
 
-var signedTransaction = undefined;
+let signedTransaction;
 
-var sentTransaction = undefined;
+let sentTransaction;
 
-var sentTransactionInfo = undefined;
+let sentTransactionInfo;
 
-var bip44Path = undefined;
+let bip44Path;
 
-var getLedgerDeviceInfo = function() {
-    process.stdout.write( "getLedgerDeviceInfo \n" );
+let getLedgerDeviceInfo = () => {
+    process.stdout.write( 'getLedgerDeviceInfo \n' );
 
-    comm_node.list_async().then( function( result ) {
-        if ( result.length == 0 ) {
-            process.stdout.write( "getLedgerDeviceInfo \"No device found\"\n" );
-            ledgerDeviceInfo = "Failure : No device found";
+    comm_node.list_async().then( result => {
+        if ( !result.length ) {
+            process.stdout.write( 'getLedgerDeviceInfo "No device found"\n' );
+            ledgerDeviceInfo = 'Failure : No device found';
             renderApp();
         } else {
-            comm_node.create_async().then( function( comm ) {
-                var deviceInfo = comm.device.getDeviceInfo();
+            comm_node.create_async().then( comm => {
+                let deviceInfo = comm.device.getDeviceInfo();
                 comm.device.close();
 
-                ledgerDeviceInfo = "Success: " + JSON.stringify( deviceInfo );
+                ledgerDeviceInfo = 'Success: ' + JSON.stringify( deviceInfo );
                 renderApp();
                 setAllLedgerInfoTimer();
             } )
-                .catch( function( reason ) {
+                .catch( reason => {
                     comm.device.close();
-                    ledgerDeviceInfo = "An error occured: " + JSON.stringify( reason );
+                    ledgerDeviceInfo = 'An error occured: ' + JSON.stringify( reason );
                     renderApp();
                     setAllLedgerInfoTimer();
                 } );
         }
     } );
-}
+};
 
-var getPublicKeyInfo = function() {
+let getPublicKeyInfo = () => {
     publicKey = undefined;
     publicKeyInfo = undefined;
-    comm_node.create_async().then( function( comm ) {
-        var message = Buffer.from( "8004000000" + bip44_path, "hex" );
-        var validStatus = [0x9000];
-        comm.exchange( message.toString( "hex" ), validStatus ).then( function( response ) {
+    comm_node.create_async().then( comm => {
+        let message = Buffer.from( '8004000000' + bip44_path, 'hex' );
+        let validStatus = [0x9000];
+        comm.exchange( message.toString( 'hex' ), validStatus ).then( response => {
             comm.device.close();
 
-            // process.stdout.write( "Public Key Response [" + response.length + "] " + response + "\n" );
+            // process.stdout.write( 'Public Key Response [' + response.length + '] ' + response + '\n' );
 
-            var publicKey = response.substring( 0, 130 );
+            let publicKey = response.substring( 0, 130 );
 
-            process.stdout.write( "Public Key [" + publicKey.length + "] " + publicKey + "\n" );
+            process.stdout.write( `Public Key [${ publicKey.length }] ${ publicKey }\n` );
 
             publicKeyInfo = publicKey;
 
             renderApp();
             setAllLedgerInfoTimer();
-        } ).catch( function( reason ) {
+        } ).catch( reason => {
             comm.device.close();
-            process.stdout.write( "error reason " + reason + "\n" );
-            publicKeyInfo = "An error occured[1]: " + reason;
+            process.stdout.write( `error reason ${ reason }\n` );
+            publicKeyInfo = 'An error occured[1]: ' + reason;
             renderApp();
             setAllLedgerInfoTimer();
         } );
     } )
-        .catch( function( reason ) {
+        .catch( reason => {
             comm.device.close();
-            process.stdout.write( "error reason " + reason + "\n" );
-            publicKeyInfo = "An error occured[2]: " + reason;
+            process.stdout.write( `error reason ${ reason }\n` );
+            publicKeyInfo = 'An error occured[2]: ' + reason;
             renderApp();
             setAllLedgerInfoTimer();
         } );
-}
+};
 
-var allLedgerInfoPollIx = 0;
+let allLedgerInfoPollIx = 0;
 
-var setAllLedgerInfoTimer = function() {
+let setAllLedgerInfoTimer = () => {
     setImmediate( getAllLedgerInfo );
-}
+};
 
-var getAllLedgerInfo = function() {
-    process.stdout.write( "getAllLedgerInfo " + allLedgerInfoPollIx + "\n" );
-    var resetPollIndex = false;
+let getAllLedgerInfo = () => {
+    process.stdout.write( `getAllLedgerInfo ${ allLedgerInfoPollIx }\n` );
+    let resetPollIndex = false;
     switch ( allLedgerInfoPollIx ) {
         case 0:
             getLedgerDeviceInfo();
@@ -154,14 +154,13 @@ var getAllLedgerInfo = function() {
 
 setAllLedgerInfoTimer();
 
-var App = React.createClass( {
+let App = React.createClass( {
 
-    getInitialState: function() {
-        return {
-        };
+    getInitialState: () => {
+        return {};
     },
 
-    render: function() {
+    render: () => {
         return (
             <Grid>
                 <h3>Ledger NodeJS</h3>
@@ -169,8 +168,8 @@ var App = React.createClass( {
                     <Col>
                         Public Key
                         <p style={{ wordBreak: 'break-all' }}><code>{publicKeyInfo}</code></p>
-                        <p>Destination Address <input type="text" size="40" id="toAddress" placeholder="Destination Address" /></p>
-                        <p>Amount <input type="number" step="0.000001" min="1" id="amount" size="30" /></p>
+                        <p>Destination Address <input type='text' size='40' id='toAddress' placeholder='Destination Address' /></p>
+                        <p>Amount <input type='number' step='0.000001' min='1' id='amount' size='30' /></p>
                         <p>
                             <button onClick={( e ) => encodeTransaction()}>Encode</button>
                         </p>
@@ -200,20 +199,18 @@ var App = React.createClass( {
     }
 } );
 
-var createSignature = function() {
-    var text = encodeTransactionResponse;
-
-    var textToSign = text + bip44_path;
+let createSignature = () => {
+    let textToSign = encodeTransactionResponse + bip44_path;
 
     signature = undefined;
-    signatureInfo = "Ledger Signing Text of Length [" + textToSign.length + "], Please Confirm Using the Device's Buttons. " + textToSign;
+    signatureInfo = `Ledger Signing Text of Length [${ textToSign.length }], Please Confirm Using the Device's Buttons. ${ textToSign }`;
     renderApp();
 
-    process.stdout.write( signatureInfo + "\n" );
+    process.stdout.write( signatureInfo + '\n' );
 
-    var validStatus = [0x9000];
+    let validStatus = [0x9000];
 
-    var messages = [];
+    let messages = [];
 
     let bufferSize = 255 * 2;
     let offset = 0;
@@ -225,48 +222,46 @@ var createSignature = function() {
         } else {
             chunk = textToSign.substring( offset );
         }
-        if ( ( offset + chunk.length ) == textToSign.length ) {
-            p1 = "80";
+        if ( ( offset + chunk.length ) === textToSign.length ) {
+            p1 = '80';
         } else {
-            p1 = "00";
+            p1 = '00';
         }
 
         let chunkLength = chunk.length / 2;
 
-        process.stdout.write( "Ledger Signature chunkLength " + chunkLength + "\n" );
+        process.stdout.write( `Ledger Signature chunkLength ${ chunkLength }\n` );
 
         let chunkLengthHex = chunkLength.toString( 16 );
         while ( chunkLengthHex.length < 2 ) {
-            chunkLengthHex = "0" + chunkLengthHex;
+            chunkLengthHex = '0' + chunkLengthHex;
         }
 
-        process.stdout.write( "Ledger Signature chunkLength hex " + chunkLengthHex + "\n" );
+        process.stdout.write( `Ledger Signature chunkLength hex ${ chunkLengthHex }\n` );
 
-        messages.push( "8002" + p1 + "00" + chunkLengthHex + chunk );
+        messages.push( '8002' + p1 + '00' + chunkLengthHex + chunk );
         offset += chunk.length;
     }
 
-    comm_node.create_async( 0, false ).then( function( comm ) {
+    comm_node.create_async( 0, false ).then( comm => {
         for ( let ix = 0; ix < messages.length; ix++ ) {
             let message = messages[ix];
-            process.stdout.write( "Ledger Message (" + ix +
-                "/" + messages.length +
-                ") " + message + "\n" );
+            process.stdout.write( `Ledger Message (${ ix }/${ messages.length }) ${ message }\n` );
 
-            comm.exchange( message, validStatus ).then( function( response ) {
-                process.stdout.write( "Ledger Signature Response " + response + "\n" );
-                if ( response != "9000" ) {
+            comm.exchange( message, validStatus ).then( response => {
+                process.stdout.write( `Ledger Signature Response ${ response }\n` );
+                if ( response !== '9000' ) {
                     comm.device.close();
 
                     /**
                      * https://stackoverflow.com/questions/25829939/specification-defining-ecdsa-signature-data
                      * <br>
                      * the signature is TLV encoded.
-                     * the first byte is 30, the "signature" type<br>
+                     * the first byte is 30, the 'signature' type<br>
                      * the second byte is the length (always 44)<br>
-                     * the third byte is 02, the "number: type<br>
+                     * the third byte is 02, the 'number: type<br>
                      * the fourth byte is the length of R (always 20)<br>
-                     * the byte after the encoded number is 02, the "number: type<br>
+                     * the byte after the encoded number is 02, the 'number: type<br>
                      * the byte after is the length of S (always 20)<br>
                      * <p>
                      * eg:
@@ -276,68 +271,66 @@ var createSignature = function() {
                      */
 
                     let rLenHex = response.substring( 6, 8 );
-                    // process.stdout.write( "Ledger Signature rLenHex " + rLenHex + "\n" );
+                    // process.stdout.write( 'Ledger Signature rLenHex ' + rLenHex + '\n' );
                     let rLen = parseInt( rLenHex, 16 ) * 2;
-                    // process.stdout.write( "Ledger Signature rLen " + rLen + "\n" );
+                    // process.stdout.write( 'Ledger Signature rLen ' + rLen + '\n' );
                     let rStart = 8;
-                    // process.stdout.write( "Ledger Signature rStart " + rStart + "\n" );
+                    // process.stdout.write( 'Ledger Signature rStart ' + rStart + '\n' );
                     let rEnd = rStart + rLen;
-                    // process.stdout.write( "Ledger Signature rEnd " + rEnd + "\n" );
+                    // process.stdout.write( 'Ledger Signature rEnd ' + rEnd + '\n' );
 
-                    while ( ( response.substring( rStart, rStart + 2 ) == "00" ) && ( ( rEnd - rStart ) > 64 ) ) {
+                    while ( ( response.substring( rStart, rStart + 2 ) === '00' ) && ( ( rEnd - rStart ) > 64 ) ) {
                         rStart += 2;
                     }
 
                     let r = response.substring( rStart, rEnd );
-                    process.stdout.write( "Ledger Signature R [" + rStart + "," + rEnd + "]:" + ( rEnd - rStart ) + " " + r + "\n" );
+                    process.stdout.write( `Ledger Signature R [${ rStart },${ rEnd }]:${ rEnd - rStart } ${ r }\n` );
                     let sLenHex = response.substring( rEnd + 2, rEnd + 4 );
-                    // process.stdout.write( "Ledger Signature sLenHex " + sLenHex + "\n" );
+                    // process.stdout.write( 'Ledger Signature sLenHex ' + sLenHex + '\n' );
                     let sLen = parseInt( sLenHex, 16 ) * 2;
-                    // process.stdout.write( "Ledger Signature sLen " + sLen + "\n" );
+                    // process.stdout.write( 'Ledger Signature sLen ' + sLen + '\n' );
                     let sStart = rEnd + 4;
-                    // process.stdout.write( "Ledger Signature sStart " + sStart + "\n" );
+                    // process.stdout.write( 'Ledger Signature sStart ' + sStart + '\n' );
                     let sEnd = sStart + sLen;
-                    // process.stdout.write( "Ledger Signature sEnd " + sEnd + "\n" );
+                    // process.stdout.write( 'Ledger Signature sEnd ' + sEnd + '\n' );
 
-                    while ( ( response.substring( sStart, sStart + 2 ) == "00" ) && ( ( sEnd - sStart ) > 64 ) ) {
+                    while ( ( response.substring( sStart, sStart + 2 ) === '00' ) && ( ( sEnd - sStart ) > 64 ) ) {
                         sStart += 2;
                     }
 
                     let s = response.substring( sStart, sEnd );
-                    process.stdout.write( "Ledger Signature S [" + sStart + "," + sEnd + "]:" + ( sEnd - sStart ) + " " + s + "\n" );
+                    process.stdout.write( `Ledger Signature S [${ sStart },${ sEnd }]:${ sEnd - sStart } ${ s }\n` );
 
                     let msgHashStart = sEnd + 4;
                     let msgHashEnd = msgHashStart + 64;
                     let msgHash = response.substring( msgHashStart, msgHashEnd );
-                    process.stdout.write( "Ledger Signature msgHash [" + msgHashStart + "," + msgHashEnd + "] " + msgHash + "\n" );
+                    process.stdout.write( `Ledger Signature msgHash [${ msgHashStart },${ msgHashEnd }] ${ msgHash }\n` );
 
                     signature = r + s;
-                    signatureInfo = "Signature of Length [" + signature.length + "] : " + signature;
-                    process.stdout.write( signatureInfo + "\n" );
+                    signatureInfo = `Signature of Length [${ signature.length }] : ${ signature }`;
+                    process.stdout.write( signatureInfo + '\n' );
 
-                    process.stdout.write( "Check Signature of Length [" + checkSignature.length + "] : " + checkSignature + "\n" );
+                    process.stdout.write( `Check Signature of Length [${ checkSignature.length }] : ${ checkSignature }\n` );
                     renderApp();
                 }
             } )
-                .catch( function( reason ) {
+                .catch( reason => {
                     comm.device.close();
-                    signatureInfo = "An error occured[1]: " + reason;
-                    process.stdout.write( "Signature Reponse " + signatureInfo + "\n" );
+                    signatureInfo = 'An error occured[1]: ' + reason;
+                    process.stdout.write( `Signature Reponse ${ signatureInfo }\n` );
                     renderApp();
                 } );
         }
     } )
-        .catch( function( reason ) {
+        .catch( reason => {
             comm.device.close();
-            signatureInfo = "An error occured[2]: " + reason;
-            process.stdout.write( "Signature Reponse " + signatureInfo + "\n" );
+            signatureInfo = 'An error occured[2]: ' + reason;
+            process.stdout.write( `Signature Reponse ${ signatureInfo }\n` );
             renderApp();
         } );
-}
-
-var renderApp = function() {
-    ReactDOM.render( <App />, document.getElementById( 'example' ) );
 };
+
+let renderApp = () => ReactDOM.render( <App />, document.getElementById( 'example' ) );
 
 renderApp();
 
